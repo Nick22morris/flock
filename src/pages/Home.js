5bcf25ec0820b973431cb19b34c233d593894947
nick22morris <nick88morris22@gmail.com>
2023-05-19 14:42:58 -0500

    redo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { getDocs, collection, query, orderBy } from "firebase/firestore";
+import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
-
 function Home({ isAuth }) {
   const [latestPost, setLatestPost] = useState(null);
   const [allPosts, setAllPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
-  var postsCollectionRef = collection(db, "posts");
-  const q = query(postsCollectionRef, orderBy("timeStamp"));
-  postsCollectionRef = q;
+  const postsCollectionRef = collection(db, "posts");
   let navigate = useNavigate();
-
   useEffect(() => {
     const getPosts = async () => {
       console.log("attempt");
@@ -25,40 +19,26 @@ function Home({ isAuth }) {
 
     getPosts();
   }, []);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = allPosts.slice(indexOfFirstPost, indexOfLastPost);
-
-  function paginate(pageNumber) {
-    setCurrentPage(pageNumber);
-  }
-
   function goToPost(title, body) {
     localStorage.setItem("clickedTitle", title);
     localStorage.setItem("clickedBody", body);
     navigate("/post");
   }
-
   return (
     <div className="homePage">
       <article className="featured">
         <h2>Latest - {latestPost ? latestPost.title : "Loading..."}</h2>
-        <p>
-          <div dangerouslySetInnerHTML={{ __html: latestPost ? latestPost.postText.substring(0, 400) + "..." : "Loading" }} />
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: latestPost ? latestPost.postText.substring(0, 400) + "..." : "Loading" }} />
         <button className="button" onClick={() => goToPost(latestPost.title, latestPost.postText)}>
           Read More
         </button>
       </article>
 
-      {currentPosts.slice(1).map((post) => {
+      {allPosts.slice(1).map((post) => {
         return (
-          <article key={post.id}>
+          <article>
             <h2>{post.title ? post.title : "Loading..."}</h2>
-            <p>
-              <div dangerouslySetInnerHTML={{ __html: post.postText ? post.postText.substring(0, 400) + "..." : "Loading" }} />
-            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.postText ? post.postText.substring(0, 400) + "..." : "Loading" }} />
             <button className="button" onClick={() => goToPost(post.title, post.postText)}>
               Read More
             </button>
